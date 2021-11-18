f9cb60ce1d367e575182560432832290ac01c5cd
olezhansky <fenix12895@gmail.com>
2021-11-18 22:30:10 +0200

    commit

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,6 +5,7 @@ import ErrorPage from '../pages/ErrorPage'
 import Main from '../pages/Main'
 import Thanks from '../pages/Thanks'
 import TeachersPage from '../pages/TeachersPage'
+import GoogleAnalytics from '../components/GoogleAnalytics/GoogleAnalytics'
 
 const Routes = () => (
   <Switch>
@@ -14,6 +15,7 @@ const Routes = () => (
     <Route exact path="/thanks" component={Thanks} />
     <Route path="/404" component={ErrorPage} />
     <Redirect from="*" to="/404" />
+    <GoogleAnalytics />
   </Switch>
 )
 
