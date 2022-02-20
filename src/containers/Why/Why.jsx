b5ae178c2e0baf2a36f6723a273c32846e2d900b
olezhansky <fenix12895@gmail.com>
2022-02-20 20:42:30 +0200

    commit

--- src/containers/Why/Why.jsx
+++ src/containers/Why/Why.jsx
@@ -4,6 +4,8 @@ import classes from './Why.module.scss'
 import { whyData } from '../../data/data';
 import { useTranslation } from 'react-i18next';
 import { useMediaQuery } from '@material-ui/core';
+import imgTest from '../../assets/icons/whyIcon.svg'
+import { Parallax } from 'react-parallax';
 
 const Why = () => {
     const { t } = useTranslation();
@@ -12,40 +14,40 @@ const Why = () => {
     return (
         <div className={classes.Wrapper}>
             <div className="container">
-                <div className={classes.Inner}>
-                    <div className={classes.Title}>
-                        <Title title={t('why.title')}/>
-                    </div>
-                    <ul className={classes.List}>
-                        {whyData.map((item) => {
-                            if (tabletMatch) {
-                                return (
-                                    <li 
-                                        key={item.id} 
-                                        className={classes.Item} 
-                                        data-aos={item.aos}
-                                    >
-                                        <img src={item.img} alt="img"/>
-                                        <p>{t(item.title)}</p>
-                                    </li>
-                                )
-                            } else if ( mobileMatch && !tabletMatch) {
-                                return (
-                                    <li 
-                                        key={item.id} 
-                                        className={classes.Item} 
-                                        data-aos={item.aosMobile} 
-                                        data-aos-duration={item.durationMobile}
-                                    >
-                                        <img src={item.img} alt="img"/>
-                                        <p>{t(item.title)}</p>
-                                    </li>
-                                ) 
-                            }
-                            return null
-                        })}
-                    </ul>
-                </div>
+                <Parallax blur={0} bgImage={imgTest} bgImageStyle={{width: '80%', height: '80%', marginTop: 150}} bgImageAlt="the cat" strength={300}>
+                        <div className={classes.Title}>
+                            <Title title={t('why.title')}/>
+                        </div>
+                        <ul className={classes.List}>
+                            {whyData.map((item) => {
+                                if (tabletMatch) {
+                                    return (
+                                        <li 
+                                            key={item.id} 
+                                            className={classes.Item} 
+                                            data-aos={item.aos}
+                                        >
+                                            <img src={item.img} alt="img"/>
+                                            <p>{t(item.title)}</p>
+                                        </li>
+                                    )
+                                } else if ( mobileMatch && !tabletMatch) {
+                                    return (
+                                        <li 
+                                            key={item.id} 
+                                            className={classes.Item} 
+                                            data-aos={item.aosMobile} 
+                                            data-aos-duration={item.durationMobile}
+                                        >
+                                            <img src={item.img} alt="img"/>
+                                            <p>{t(item.title)}</p>
+                                        </li>
+                                    ) 
+                                }
+                                return null
+                            })}
+                        </ul>
+                    </Parallax>
             </div>
         </div>
     )
